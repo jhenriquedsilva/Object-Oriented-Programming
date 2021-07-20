fe9131261e446b0c22e28b0407641d4579972e5d
jhenriquedsilva <josehs555@gmail.com>
2021-07-19 22:14:54 -0300

    A little bit about classes

---
+++ src/exercise_two/Principal.java
@@ -0,0 +1,14 @@
+package exercise_two;
+
+public class Principal {
+    public static void main(String args[]) {
+        // Questão 3
+        System.out.println("Questão 3");
+
+        // Questão 4
+        System.out.println("\nQuestão 4");
+
+        // Questão 5
+        System.out.println("\nQuestão 5");
+    }
+}
